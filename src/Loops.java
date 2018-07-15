5fa8af86e5dc57b30c0bf33437fa7b0826cbcbcb
bryjoseph44 <bryanmatthewjoseph@gmail.com>
2018-07-14 22:07:17 -0600

    updating with latest from lessons -- last of user input challenge

--- src/Loops.java
+++ src/Loops.java
@@ -133,6 +133,9 @@ public class Loops {
 
         // code test 17
         System.out.println(getLargestPrime(0));
+
+        // code test 18
+        printSquareStar(10);
     }
 
 
@@ -465,5 +468,21 @@ public class Loops {
             }
         }
         return p;
+    }
+
+    public static void printSquareStar(int number) {
+        if(number < 5) {
+            System.out.println("Invalid value");
+            return;
+        }
+        for (int i = 1; i <= number; i++)  {
+            String s = "";
+            for (int j = 1; j <= number; j++)  {
+                s += ((i==1) || (i==number)                     // top and bottom rows
+                ||    (j==1) || (j==number)                     // left and right edges
+                ||    (j==i) || (j==(number-i+1)))? "*" : " ";  // stars in the middle
+            }
+            System.out.println(s);
+        }
     }
 }
--- src/com/userInput/Main.java
+++ src/com/userInput/Main.java
@@ -9,18 +9,85 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character ENTER key
+// first example of using the scanner class to read user input
+//        System.out.println("Enter your year of birth:");
+//
+//        boolean hasNextInt = scanner.hasNextInt();
+//
+//        if(hasNextInt) {
+//            int yearOfBirth = scanner.nextInt();
+//            scanner.nextLine(); // handle next line character ENTER key and get to the next question
+//
+//            System.out.println("Enter your name: ");
+//            String name = scanner.nextLine(); // handle the name
+//
+//            int age = 2018 - yearOfBirth;
+//
+//            if( age >= 0 && age <= 100) {
+//                System.out.println("You entered " + name + "." + " Hello " + name + "." + " You are " + age);
+//            } else {
+//                System.out.println("Invalid year of birth");
+//            }
+//        } else {
+//            System.out.println("Unable to parse year of birth");
+//        }
+//        scanner.close();
 
+    // start user input challenge
+//        int counter = 1;
+////      int sum = 0;
+////
+////        while (counter < 11) {
+////            System.out.println("Enter number for #" + counter + ":" + "\n" + "Current total = " + sum);
+////
+////            boolean hasNextInt = scanner.hasNextInt();
+////
+////            if (hasNextInt) {
+////                int enteredNumber = scanner.nextInt();
+////                sum += enteredNumber;
+////                counter++;
+////            } else {
+////                System.out.println("Invalid number");
+////            }
+////            scanner.nextLine();
+////        }
+////        System.out.println("Total is : " + sum);
+////        scanner.close();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+    // third challenge Min and Max  for user input
+    int min = 0;
+    int max = 0;
+    int counter = 1;
+    boolean first = true;
 
-        int age = 2018 - yearOfBirth;
+    while(counter < 11) {
+        System.out.println("Enter number for #" + counter + ":");
 
-        System.out.println("You entered " + name + "." + " Hello " + name + "." + " You are " + age);
+        boolean isInt = scanner.hasNextInt();
 
-        scanner.close();
+        if(isInt) {
+            int enteredNumber = scanner.nextInt();
+
+            if(first) {
+                min = enteredNumber;
+                max = enteredNumber;
+                first = false;
+            }
+
+            if(enteredNumber > 0 && enteredNumber < max) {
+                min = enteredNumber;
+            } else if (enteredNumber > max) {
+                max = enteredNumber;
+            } else {
+                scanner.nextLine();
+            }
+        } else {
+            System.out.println("Not a valid number, please try again");
+            scanner.nextLine();
+        }
+        System.out.println("Current min number is : " + min + " \nCurrent max is : " + max);
+        counter++;
+    }
+    scanner.close();
     }
 }
